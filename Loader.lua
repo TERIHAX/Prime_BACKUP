b5021b03589caa9efdea166d7d9c86bbfd303480
Space <94127295+SpaceYes@users.noreply.github.com>
2023-02-12 15:47:37 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,25 @@
---
+local MaintenanceTeam = {
+	[4203535728] = true,
+	[4203545688] = true,
+}
+
+local SupportedGames = {
+	[2788229376] = true,
+	[7213786345] = true,
+}
+
+local Maintenance = true
+
+if not MaintenanceTeam[game:GetService("Players").LocalPlayer.UserId] then
+	if Maintenance ~= false then
+		game:GetService("Players").LocalPlayer:Kick("Prime is Currently on Maintenance [ Error Code : 0xA ]")
+		return
+	end
+end
+
+if SupportedGames[game.PlaceId] then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/SpaceYes/Roblox/Main/Obfuscated.lua"))();
+	return
+end
+
+game:GetService("Players").LocalPlayer:Kick("This Game is Currently Unsupported! [ Error Code : 0xB ]")
